bd57c16c02c41be46b3c9cf9f604942e68979615
Soumyajit Basu (C0r3f!nd3r) <soumyajit.basu62@gmail.com>
2020-08-27 21:48:16 +0530

    change formatting for data 	modified:   test/test_data.py

--- test/test_data.py
+++ test/test_data.py
@@ -1,3 +1,4 @@
+# ---------------------------------------input data--------------------------------------------
 class Testdata:
     data_set = [
         ("zip_code", "bengaluru"), ("city_name", "kolkata"), ("city_id", "bengaluru"), ("co-ordinates", "kolkata")
