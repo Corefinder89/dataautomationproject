ca966d169a3e8d47c549c95ce507f079b09df750
Soumyajit Basu (C0r3f!nd3r) <soumyajit.basu62@gmail.com>
2020-08-28 15:59:38 +0530

    Update readme and change test data 	modified:   readme.md 	modified:   test/test_data.py

--- test/test_data.py
+++ test/test_data.py
@@ -3,5 +3,5 @@ class Testdata:
     data_set = [
         ('{"lat":"12.91","lon":"77.63"}', "bengaluru"), ('{"q":"bengaluru,in"}', "bengaluru"),
         ('{"id":"1277333"}', "bengaluru"), ('{"zip":"560068,in"}', "bengaluru"), ('{"q":"kolkata,in"}', "kolkata"),
-        ('{"zip":"700048,in"}', "kolkata")
+        ('{"zip":"700048,in"}', "kolkata"), ('{"lat":"abcd","lon":"def"}', "bengaluru")
     ]
